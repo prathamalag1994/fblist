dfa0897bf95f4e5236161f1a78bd177bab3a3232
prathamalag1994 <prathamalag@berkeley.edu>
2015-02-22 15:05:08 -0800

    make it work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
 
   def index
   	oauth = Koala::Facebook::OAuth.new(ENV["1528582454074095"], ENV["8133e2fc26bb995d7d4eb4d4b6ab7fd9"])
-  	oauth_access_token = 'CAACEdEose0cBACvE9Iijf1WdZB2AT3uH7NFh44Y3iY9y4Fiu9Ij1E3IfGvptKoH9m2G4PZCOo7EZBefaJQksBBu7P4seI5BNQdJ7wC50DstwZCIPiXqjZBaW8Tsm9tJXad0EbM0yZAfJxZBSVfbYYKo0xDZClkKtBUKtoYv8EbKlyL4bkpDwYOYvKwlh102GwGlh8G1WrGrbs3Qi13yBVHC15Hk1W6XvoXUZD'
+  	oauth_access_token = 'CAACEdEose0cBAKZCqm7LvyMqw5DWQVEN9XKHG3L39jBgLn6HWqwoVUVhMmav05Vz14DX9EZBsuIvUtVBRPWGCMNqiS2gTZCYlUePctqDMzNxrifhQVjQuTMQN1R8y5lxxV2pnI4QrQ7P7qFoPf8eZCoHUW37sI4Er5h2L43vrULnfA0Ho8ogaAmAEsFd91KMUKcAvYaIq63Ej4uX91WGQV0WAXdHyb8ZD'
 
 	@graph = Koala::Facebook::API.new(oauth_access_token)
   	@events = @graph.get_connections("me", "events")
